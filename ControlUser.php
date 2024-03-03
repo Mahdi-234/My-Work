3fc4764203d54da7e3dd1440fee924a8930d3cbe
Mahdi-234 <mahdihadiji123@gmail.com>
2024-03-03 16:09:54 +0100

    OK

--- ControlUser.php
+++ ControlUser.php
@@ -1,6 +1,19 @@
 <?php
 
-include 
+include ("modele.php");
+$l=$_POST['login'];
+$p=$_POST['mdp'];
+$u=new user($l,$p);
+$va=$u->connect();
+if($va)
+{
+    echo $u ;
+}
+else
+{
+    header("Location:Authentification.html");
+
+}
 
 
 
